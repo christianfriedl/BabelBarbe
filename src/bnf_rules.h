b47273366616ea9e559d86d46b99d2725e497d62
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2011-10-26 19:23:40 +0200

    declare rule variables

--- src/bnf_rules.h
+++ src/bnf_rules.h
@@ -1,4 +1,22 @@
 #ifndef _BNF_RULES_H
 #define _BNF_RULES_H
 
+extern rule_t rule_start;
+extern rule_t rule_rules;
+extern rule_t rule_rule;
+extern rule_t rule_rule_lhs;
+extern rule_t rule_rule_rhs;
+extern rule_t rule_rule_phrase;
+extern rule_t rule_term;
+extern rule_t rule_openparen;
+extern rule_t rule_closeparen;
+extern rule_t rule_repetition_marker;
+extern rule_t rule_symbol;
+extern rule_t rule_keyword_regex;
+extern rule_t rule_literal;
+extern rule_t rule_definition_sign;
+extern rule_t rule_semicolon;
+extern rule_t rule_identifier;
+extern rule_t rule_terminal;
+extern rule_t rule_nonterminal;
 #endif
