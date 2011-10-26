c5c51e082f9a7a4083cf469d98508ed1f5477ee0
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2011-10-26 19:22:33 +0200

    move repeat markers to correct place

--- src/bnf_rules.c
+++ src/bnf_rules.c
@@ -6,7 +6,6 @@ rule_t rule_start = {
     "rule_start",
     NULL,
     NULL,
-    repeat_off, 0,
     {
         { repeat_off, 0, { &rule_rules, NULL } },
         NULL_RULE
@@ -17,9 +16,8 @@ rule_t rule_rules = {
     "rule_rules",
     NULL,
     NULL,
-    repeat_on, 0,
     {
-        { & rule_rule, NULL },
+        { repeat_on, 0, { & rule_rule, NULL } },
         NULL_RULE
     }
 };
@@ -28,9 +26,8 @@ rule_t rule_rule = {
     "rule_rule",
     NULL,
     NULL,
-    repeat_off, 0,
     {
-        { &rule_rule_lhs, &rule_definition_sign, &rule_rule_rhs, &rule_semicolon, NULL },
+        { repeat_off, 0, { &rule_rule_lhs, &rule_definition_sign, &rule_rule_rhs, &rule_semicolon, NULL } },
         NULL_RULE
     }
 };
@@ -39,9 +36,8 @@ rule_t rule_rule_lhs = {
     "rule_rule_lhs",
     NULL,
     NULL,
-    repeat_off, 0,
     {
-        { &rule_identifier, NULL },
+        { repeat_off, 0, { &rule_identifier, NULL } },
         NULL_RULE
     }
 };
@@ -50,9 +46,8 @@ rule_t rule_rule_rhs = {
     "rule_rule_rhs",
     NULL,
     NULL,
-    repeat_on, 0,
     {
-        { &rule_rule_phrase, NULL },
+        { repeat_on, 0, { &rule_rule_phrase, NULL } },
         NULL_RULE
     }
 };
@@ -61,21 +56,20 @@ rule_t rule_rule_phrase = {
     "rule_rule_phrase",
     NULL,
     NULL,
-    repeat_off, 0,
     {
-        { &rule_term, &rule_repetition_marker, NULL },
-        { &rule_term, NULL },
+        { repeat_off, 0, { &rule_term, &rule_repetition_marker, NULL } },
+        { repeat_off, 0, { &rule_term, NULL } },
         NULL_RULE
+    }
 };
 
 rule_t rule_term = {
     "rule_term",
     NULL,
     NULL,
-    repeat_off, 0,
     {
-        { &rule_symbol, NULL },
-        { &rule_openparen, &rule_rule_rhs, &rule_closeparen, NULL },
+        { repeat_off, 0, { &rule_symbol, NULL } },
+        { repeat_off, 0, { &rule_openparen, &rule_rule_rhs, &rule_closeparen, NULL } },
         NULL_RULE
     }
 };
@@ -84,7 +78,6 @@ rule_t rule_openparen = {
     "rule_openparen",
     parser__parse_openparen,
     NULL,
-    repeat_off, 0,
     {
         NULL_RULE
     }
@@ -94,7 +87,6 @@ rule_t rule_closeparen = {
     "rule_closeparen",
     parser__parse_closeparen,
     NULL,
-    repeat_off, 0,
     {
         NULL_RULE
     }
@@ -104,7 +96,6 @@ rule_t rule_repetition_marker = {
     "rule_repetition_marker",
     parser__parse_repetition_marker,
     NULL,
-    repeat_off, 0,
     {
         NULL_RULE
     }
@@ -114,10 +105,9 @@ rule_t rule_symbol = {
     "rule_symbol",
     NULL,
     NULL,
-    repeat_off, 0,
     {
-        { &rule_terminal, NULL },
-        { &rule_nonterminal, NULL },
+        { repeat_off, 0, { &rule_terminal, NULL } },
+        { repeat_off, 0, { &rule_nonterminal, NULL } },
         NULL_RULE
     }
 };
@@ -126,18 +116,17 @@ rule_t rule_terminal = {
     "rule_terminal",
     NULL,
     NULL,
-    repeat_off, 0,
     {
-        { &rule_keyword_regex, &rule_literal, NULL },
-        { &rule_literal, NULL }
+        { repeat_off, 0, { &rule_keyword_regex, &rule_literal, NULL } },
+        { repeat_off, 0, { &rule_literal, NULL  } },
+        NULL_RULE
     }
 };
 
 rule_t rule_keyword_regex = {
     "rule_keyword_regex",
-    parser__parse_regex,
+    parser__parse_keyword_regex,
     NULL,
-    repeat_off, 0,
     {
         NULL_RULE
     }
@@ -147,7 +136,30 @@ rule_t rule_literal = {
     "rule_literal",
     parser__parse_literal,
     NULL,
-    repeat_off, 0,
+    {
+        NULL_RULE
+    }
+};
+rule_t rule_definition_sign = {
+    "rule_definition_sign",
+    parser__parse_definition_sign,
+    NULL,
+    {
+        NULL_RULE
+    }
+};
+rule_t rule_semicolon = {
+    "rule_semicolon",
+    parser__parse_semicolon,
+    NULL,
+    {
+        NULL_RULE
+    }
+};
+rule_t rule_identifier = {
+    "rule_identifier",
+    parser__parse_identifier,
+    NULL,
     {
         NULL_RULE
     }
