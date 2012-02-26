c71a21ee943ca7e7c4af64d9b99b1619fa6d6692
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2012-02-26 16:20:14 +0100

    basic ast

--- src/BNFAst.c
+++ src/BNFAst.c
@@ -1,69 +1,20 @@
+#include<cgenerics/CGTree.h>
 #include"BNF.h"
 #include"BNFAst.h"
 
-BNFAst* BNFAst__new(BNFAst* parent, Token* token, BNFAstPosition where) {
-    BNFAst* this = malloc(sizeof(*this));
-    if (this != NULL) {
-        this->parent = parent;
-        this->token = token;
-        this->left = NULL;
-        this->right = NULL;
-        if (parent != NULL) {
-            if (where == BNFAstPosition_left)
-                parent->left = this;
-            else if (where == BNFAstPosition_right)
-                parent->right = this;
-            else
-                BNF__raiseFatalError("Parent given, but no ast_pos argument");
-        }
-    } else
-        BNF__raiseFatalError("Unable to allocate BNFAst.");
+DEFINE_TREE_FUNCS(BNFToken)
+
+BNFAst* BNFAst__new(BNFAst* parent, BNFToken* token) {
+    BNFAst* this = CGTree__new(BNFToken, token);
+    if (parent != NULL)
+        CGTree_addSubTree(BNFToken, parent, this);
     return this;
 }
 void BNFAst_delete(BNFAst* this) {
-    if (this->left != NULL)
-        BNFAst_delete(this->left);
-    if (this->right != NULL)
-        BNFAst_delete(this->right);
-    Token_delete(this->token);
-    free(this);
+    CGTree_deleteValues(BNFToken, this);
+    CGTree_delete(BNFToken, this);
 }
-BNFAst* BNFAst_getParent(BNFAst* this) {
-    return this->parent;
-}
-BNFAst* BNFAst_getLeft(BNFAst* this) {
-    return this->left;
-}
-BNFAst* BNFAst_getRight(BNFAst* this) {
-    return this->right;
-}
-Token* BNFAst_getToken(BNFAst* this) {
-    return this->token;
-}
-void BNFAst_setToken(BNFAst* this, Token* token) {
-    Token_delete(this->token);
-    this->token = token;
-}
-void BNFAst_setParent(BNFAst* this, BNFAst* parent) {
-    this->parent = parent;
-}
-void BNFAst_setLeft(BNFAst* this, BNFAst* left) {
-    this->left = left;
-}
-void BNFAst_setRight(BNFAst* this, BNFAst* right) {
-    this->right = right;
-}
-void BNFAst_print_(BNFAst* this, unsigned int level);
-void BNFAst_print(BNFAst* this) {
-    BNFAst_print_(this, 0);
-}
-void BNFAst_print_(BNFAst* this, unsigned int level) {
-    unsigned int i;
-    if (this->left != NULL)
-        BNFAst_print_(this->left, level + 1);
-    for (i=0; i < level; ++i)
-        printf("    ");
-    Token_print(this->token);
-    if (this->right != NULL)
-        BNFAst_print_(this->right, level + 1);
+
+BNFToken* BNFAst_getToken(BNFAst* this) {
+    return CGTree_getValue(BNFToken, this);
 }
--- src/BNFAst.h
+++ src/BNFAst.h
@@ -1,48 +1,21 @@
-/*
-BNF Parser
+#ifndef _BNF_AST_H
+#define _BNF_AST_H
 
-Copyright (C) 2011  Christian Friedl
+#include<cgenerics/CGTree.h>
+#include"BNFToken.h"
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+/* **** PREMARK **** */
+DECLARE_TREE_TYPE(BNFToken)
+/* **** MARK **** */
+DECLARE_TREE_FUNCS(BNFToken)
+/* **** ENDMARK **** */
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+typedef CGTree(BNFToken) BNFAst;
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef _AST_H
-#define _AST_H
-
-#include"Token.h"
-
-struct BNFAst_struct;
-typedef struct BNFAst_struct {
-    Token* token;
-    struct BNFAst_struct* parent;
-    struct BNFAst_struct* left;
-    struct BNFAst_struct* right;
-} BNFAst;
-
-typedef enum { BNFAstPosition_left = 0, BNFAstPosition_right = 1, BNFAstPosition_nowhere = 2 } BNFAstPosition;
-
-BNFAst* BNFAst__new(BNFAst* parent, Token* token, BNFAstPosition where);
+BNFAst* BNFAst__new(BNFAst* parent, BNFToken* token);
 void BNFAst_delete(BNFAst* this);
+BNFToken* BNFAst_getToken(BNFAst* this);
 void BNFAst_print(BNFAst* this);
-BNFAst* BNFAst_getParent(BNFAst* this);
-BNFAst* BNFAst_getLeft(BNFAst* this);
-BNFAst* BNFAst_getRight(BNFAst* this);
-Token* BNFAst_getToken(BNFAst* this);
-void BNFAst_setToken(BNFAst* this, Token* token);
-void BNFAst_setParent(BNFAst* this, BNFAst* parent);
-void BNFAst_setLeft(BNFAst* this, BNFAst* left);
-void BNFAst_setRight(BNFAst* this, BNFAst* right);
 
 #endif
 
