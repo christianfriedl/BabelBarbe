8842cf7be528fcfb0b52ece3d383cbd6fab819ba
Christian Friedl <Mag.Christian.Friedl@gmail.com>
2012-02-28 16:44:18 +0100

    Revert "remove useless old stuff", because there is actually something useful in BNF.c
    This reverts commit 0fcbc66123db838720e3f6642cf3e10cd851f3b0.

--- src/BNF.c
+++ src/BNF.c
@@ -17,18 +17,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef _BNF_H
-#define _BNF_H
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include"BNF.h"
 
-#include"Error.h"
-
-#define bool int
-#define true (-1)
-#define false (0)
-
-#define bool__to_string(a) (((a) == true) ? "true" : "false")
-
-
-void BNF__raiseFatalError(const char* msg) __attribute((noreturn));
-
-#endif
+void BNF__raiseFatalError(const char* msg) {
+    printf("BNF FATAL ERROR: %s\n", msg);
+    abort();
+}
